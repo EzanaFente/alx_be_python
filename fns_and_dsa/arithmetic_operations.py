bd306e6dcc8afd5bafad785c0955a03cd0800ab9
EzanaFente <ezafent55e@gmail.com>
2024-07-01 11:11:25 +0200

    corrected the cases

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,26 +1,19 @@
-def perform_operation(num1,num2,operation):
-    num1=float(num1)
-    num2=float(num2)
-    operation=str(operation)
+def perform_operation(num1, num2, operation):
+    num1 = float(num1)
+    num2 = float(num2)
+    operation = str(operation).lower()
 
     match operation:
-     case "add":
-        result = num1 + num2
-        return result
-     case "subtract":
-        result = num1 - num2
-        return result
-     case "multiply":
-        result = num1 * num2
-        return result
-     case "divide":
-        if num2!=0:
-            result = num1 / num2
-            return result
-        else:
-            print("Cannot divide by zero.")
-            
-# num1=float(input("Enter the first number: "))
-# num2=float(input("Enter the second number: "))
-# operation=str(input("Enter the operation (add, subtract, multiply, divide): "))
-#perform_operation(num1,num2,operation)
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 != 0:
+                return num1 / num2
+            else:
+                return "Error: Cannot divide by zero."
+        case _:
+            return "Error: Invalid operation"
