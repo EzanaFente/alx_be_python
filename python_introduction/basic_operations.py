7e6543db6a03b56baa55e98499b6beccd697f533
EzanaFente <ezafent55e@gmail.com>
2024-07-01 10:14:19 +0200

    new line print added

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -6,3 +6,4 @@ multiplication = number1 * number2
 print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(sum))
 print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(difference))
 print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(multiplication))
+print("new line")
