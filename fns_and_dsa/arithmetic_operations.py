49bd2febdc2a5a5df2555369210c272468cfd0d4
EzanaFente <ezafent55e@gmail.com>
2024-07-01 11:03:04 +0200

    new lines added

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,17 +6,17 @@ def perform_operation(num1,num2,operation):
     match operation:
      case "add":
         result = num1 + num2
-        print(f"Result: {result}")
+        return result
      case "subtract":
         result = num1 - num2
-        print(f"Result: {result}")
+        return result
      case "multiply":
         result = num1 * num2
-        print(f"Result: {result}")
+        return result
      case "divide":
         if num2!=0:
             result = num1 / num2
-            print(f"Result: {result}")
+            return result
         else:
             print("Cannot divide by zero.")
             
