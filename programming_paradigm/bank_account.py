112d6c046178197b4529cb15ecd9b81cef46f323
EzanaFente <ezafent55e@gmail.com>
2024-07-12 18:45:16 +0200

    sdding file with a code

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,12 @@
+class BankAccount:
+    def __init__(self,balance):
+        self.balance = 0
+    def account_balance(self):
+        pass
+    def deposit(amount):
+        self.balance += amount
+    def withdraw(amount):
+        self.balance -= amount
+    def display_balance():
+        print(f"Current amount is {self.balance}")
+    
