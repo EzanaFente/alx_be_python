34e3e7726e5311efecbca76c29ef0858e5434eca
EzanaFente <ezafent55e@gmail.com>
2024-07-18 21:58:36 +0200

    changed the parameters updated

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,19 +2,22 @@ class Book:
     def __init__(self, title:str, author:str):
         self.title=title
         self.author=author
-        
+    def __str__(self):
+        return F"Book: {self.title} by {self.author}"
         
 class EBook(Book):
     def __init__(self, title:str, author:str,file_size:int):
         super().__init__(title,author)
         self.file_size=file_size
-        
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, Page Count: {self.file_size}KB"
         
 class PrintBook(Book):
     def __init__(self,title:str, author:str, page_count:int):
         super().__init__(title,author)
         self.page_count=page_count
-        
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
         
 class Library:
     def __init__(self):
@@ -23,9 +26,4 @@ class Library:
         self.books.append(book)
     def list_books(self):
         for book in self.books:
-            if isinstance(book, EBook):
-                print(f"Title: {book.title}, Author: {book.author}, File Size: {book.file_size}KB")
-            elif isinstance(book, PrintBook):
-                print(f"Title: {book.title}, Author: {book.author}, Page Count: {book.page_count}")
-            else:
-                print(f"Title: {book.title}, Author: {book.author}")
+            print(book)
